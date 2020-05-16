11331d3ef672af2ae8284e00ee646f50814865ef
Chris Laskey <contact@chrislaskey.com>
2020-05-16 09:15:44 -0400

    Delete Confirmation

--- apps/artemis_web/lib/artemis_web/view_helpers/html.ex
+++ apps/artemis_web/lib/artemis_web/view_helpers/html.ex
@@ -21,7 +21,7 @@ defmodule ArtemisWeb.ViewHelper.HTML do
   All other options are passed directly to the `Phoenix.HTML` function.
   """
   def action(label, options \\ []) do
-    basic = Keyword.get(options, :basic, "basic")
+    basic = Keyword.get(options, :basic, true) && "basic"
     color = Keyword.get(options, :color, "")
     size = Keyword.get(options, :size, "small")
     method = Keyword.get(options, :method, "get")
@@ -61,21 +61,35 @@ defmodule ArtemisWeb.ViewHelper.HTML do
   Render modal to confirm delete action
   """
   def delete_confirmation(label, to, options \\ []) do
-    color = Keyword.get(options, :color) || "basic"
+    body = Keyword.get(options, :body, nil)
+    basic = Keyword.get(options, :basic, true) && "basic"
+    color = Keyword.get(options, :color, "")
     size = Keyword.get(options, :size, "small")
+    method = Keyword.get(options, :method, :delete)
+    reason_options = Keyword.get(options, :reason_options, [])
+    title = Keyword.get(options, :title, "Confirm Delete")
     modal_id = "modal-id-#{Artemis.Helpers.UUID.call()}"
 
     button_options =
       options
+      |> Keyword.delete(:basic)
+      |> Keyword.delete(:body)
+      |> Keyword.delete(:method)
+      |> Keyword.delete(:reason_options)
+      |> Keyword.delete(:title)
       |> Keyword.delete(:to)
-      |> Keyword.put(:class, "button ui #{size} #{color} modal-trigger")
+      |> Keyword.put(:class, "button ui #{basic} #{size} #{color} modal-trigger")
       |> Keyword.put(:data, target: "##{modal_id}")
       |> Keyword.put(:to, "#delete-confirmation")
 
     assigns = [
+      body: body,
       button_label: label,
       button_options: button_options,
+      method: method,
       modal_id: modal_id,
+      reason_options: reason_options,
+      title: title,
       to: to
     ]
 
@@ -89,8 +103,16 @@ defmodule ArtemisWeb.ViewHelper.HTML do
     slug = Artemis.Helpers.generate_slug(label)
     id = "link-#{slug}"
 
+    subheading =
+      case Keyword.get(options, :subheading) do
+        nil -> ""
+        value -> content_tag(:span, value, class: "subheading")
+      end
+
     content_tag(:div, class: "heading-container h2-container", id: id) do
-      content_tag(:h2, label, options)
+      content_tag(:h2, options) do
+        [label, subheading]
+      end
     end
   end
 
@@ -133,10 +155,23 @@ defmodule ArtemisWeb.ViewHelper.HTML do
   @doc """
   Render a text input form field to capture the reason for an action
   """
-  def reason_field(form_instance) do
-    assigns = [
-      form_instance: form_instance
+  def reason_field(form_instance, options \\ []) do
+    default_options = [
+      optional: true
     ]
+
+    options = Keyword.merge(default_options, options)
+
+    placeholder =
+      case Keyword.get(options, :optional) do
+        true -> "Reason for Change (Optional)"
+        _ -> "Reason for Change"
+      end
+
+    assigns =
+      options
+      |> Keyword.put_new(:form_instance, form_instance)
+      |> Keyword.put_new(:placeholder, placeholder)
 
     Phoenix.View.render(ArtemisWeb.LayoutView, "reason_field.html", assigns)
   end
