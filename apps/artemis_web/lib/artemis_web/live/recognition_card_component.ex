55e33471462dfbed0eb701d07156490b17def492
Chris Laskey <contact@chrislaskey.com>
2020-05-29 10:52:21 -0400

    Recognition LiveView and Component Comments

--- apps/artemis_web/lib/artemis_web/live/recognition_card_component.ex
+++ apps/artemis_web/lib/artemis_web/live/recognition_card_component.ex
@@ -4,7 +4,35 @@ defmodule ArtemisWeb.RecognitionCardComponent do
   # LiveView Callbacks
 
   @impl true
+  def update(assigns, socket) do
+    socket =
+      socket
+      |> assign(:id, assigns.id)
+      |> assign(:recognition, assigns.recognition)
+      |> assign(:user, assigns.user)
+      |> assign_recognition_comments(assigns)
+
+    {:ok, socket}
+  end
+
+  @impl true
   def render(assigns) do
     Phoenix.View.render(ArtemisWeb.RecognitionView, "show/_card.html", assigns)
+  end
+
+  # Helpers
+
+  defp assign_recognition_comments(socket, %{comments: comments}) when is_list(comments) do
+    recognition_id = Integer.to_string(socket.assigns.recognition.id)
+    filtered_comments = Enum.filter(comments, &(&1.resource_id == recognition_id))
+
+    socket
+    |> assign(:recognition_comments, filtered_comments)
+    |> assign(:recognition_comments_count, length(filtered_comments))
+    |> assign(:recognition_comments_status, :loaded)
+  end
+
+  defp assign_recognition_comments(socket, _assigns) do
+    assign(socket, :recognition_comments_status, :loading)
   end
 end
--- apps/artemis_web/lib/artemis_web/live/recognition_cards_live.ex
+++ apps/artemis_web/lib/artemis_web/live/recognition_cards_live.ex
@@ -1,17 +1,25 @@
 defmodule ArtemisWeb.RecognitionCardsLive do
   use ArtemisWeb.LiveView
 
+  alias Artemis.ListComments
+
   # LiveView Callbacks
 
   @impl true
   def mount(_params, session, socket) do
     user = Map.get(session, "user")
     recognitions = Map.get(session, "recognitions")
+    broadcast_topic = Artemis.Event.get_broadcast_topic()
 
     assigns =
       socket
+      |> assign(:comments, :loading)
       |> assign(:recognitions, recognitions)
       |> assign(:user, user)
+
+    if connected?(socket), do: Process.send_after(self(), :list_comments, 10)
+
+    :ok = ArtemisPubSub.subscribe(broadcast_topic)
 
     {:ok, assigns}
   end
@@ -19,5 +27,104 @@ defmodule ArtemisWeb.RecognitionCardsLive do
   @impl true
   def render(assigns) do
     Phoenix.View.render(ArtemisWeb.RecognitionView, "show/_cards.html", assigns)
+  end
+
+  # GenServer Callbacks
+
+  @impl true
+  def handle_info(:list_comments, socket) do
+    comments = list_comments(socket)
+
+    {:noreply, assign(socket, :comments, comments)}
+  end
+
+  def handle_info(%{event: "comment:created", payload: payload}, socket) do
+    comments = maybe_add_comment(socket, payload)
+
+    {:noreply, assign(socket, :comments, comments)}
+  end
+
+  def handle_info(%{event: "comment:updated", payload: payload}, socket) do
+    comments = maybe_update_comment(socket, payload)
+
+    {:noreply, assign(socket, :comments, comments)}
+  end
+
+  def handle_info(%{event: "comment:deleted", payload: payload}, socket) do
+    comments = maybe_delete_comment(socket, payload)
+
+    {:noreply, assign(socket, :comments, comments)}
+  end
+
+  def handle_info(_, socket) do
+    {:noreply, socket}
+  end
+
+  # Helpers - GenServer Callback Events
+
+  defp list_comments(socket) do
+    params = %{
+      filters: %{
+        resource_id: get_ids(socket),
+        resource_type: "Recognition"
+      }
+    }
+
+    ListComments.call(params, socket.assigns.user)
+  end
+
+  defp maybe_add_comment(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> [payload.data | socket.assigns.comments]
+      false -> socket.assigns.comments
+    end
+  end
+
+  defp maybe_update_comment(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> update_comment(socket.assigns.comments, payload.data)
+      false -> socket.assigns.comments
+    end
+  end
+
+  defp maybe_delete_comment(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> delete_comment(socket.assigns.comments, payload.data)
+      false -> socket.assigns.comments
+    end
+  end
+
+  # Helpers
+
+  defp get_ids(socket) do
+    socket.assigns
+    |> Map.get(:recognitions)
+    |> Enum.map(&Integer.to_string(&1.id))
+  end
+
+  defp resource_type_match?(payload), do: payload.data.resource_type == "Recognition"
+
+  defp resource_id_match?(socket, payload) do
+    Enum.member?(get_ids(socket), payload.data.resource_id)
+  end
+
+  defp update_comment(current, new) do
+    Enum.map(current, fn item ->
+      case item.id == new.id do
+        true -> new
+        false -> item
+      end
+    end)
+  end
+
+  defp delete_comment(current, new) do
+    current
+    |> Enum.reduce([], fn item, acc ->
+      case item.id == new.id do
+        true -> acc
+        false -> [item | acc]
+      end
+    end)
+    |> Enum.reverse()
   end
 end
