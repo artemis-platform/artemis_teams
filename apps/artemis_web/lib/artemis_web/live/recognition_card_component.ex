4d06cf80319c469ed7f8a826f07164a5ad74678b
Chris Laskey <contact@chrislaskey.com>
2020-05-29 16:17:07 -0400

    Basic reactions integration on Recognitinos

--- apps/artemis_web/lib/artemis_web/live/recognition_card_component.ex
+++ apps/artemis_web/lib/artemis_web/live/recognition_card_component.ex
@@ -1,6 +1,8 @@
 defmodule ArtemisWeb.RecognitionCardComponent do
   use Phoenix.LiveComponent
 
+  alias Artemis.CreateReaction
+
   # LiveView Callbacks
 
   @impl true
@@ -11,6 +13,7 @@ defmodule ArtemisWeb.RecognitionCardComponent do
       |> assign(:recognition, assigns.recognition)
       |> assign(:user, assigns.user)
       |> assign_recognition_comments(assigns)
+      |> assign_recognition_reactions(assigns)
 
     {:ok, socket}
   end
@@ -18,6 +21,22 @@ defmodule ArtemisWeb.RecognitionCardComponent do
   @impl true
   def render(assigns) do
     Phoenix.View.render(ArtemisWeb.RecognitionView, "show/_card.html", assigns)
+  end
+
+  # Callbacks
+
+  @impl true
+  def handle_event("create-reaction", %{"value" => value}, socket) do
+    params = %{
+      resource_id: Integer.to_string(socket.assigns.recognition.id),
+      resource_type: "Recognition",
+      user_id: socket.assigns.user.id,
+      value: value
+    }
+
+    {:ok, _} = CreateReaction.call(params, socket.assigns.user)
+
+    {:noreply, socket}
   end
 
   # Helpers
@@ -34,5 +53,19 @@ defmodule ArtemisWeb.RecognitionCardComponent do
 
   defp assign_recognition_comments(socket, _assigns) do
     assign(socket, :recognition_comments_status, :loading)
+  end
+
+  defp assign_recognition_reactions(socket, %{reactions: reactions}) when is_list(reactions) do
+    recognition_id = Integer.to_string(socket.assigns.recognition.id)
+    filtered_reactions = Enum.filter(reactions, &(&1.resource_id == recognition_id))
+
+    socket
+    |> assign(:recognition_reactions, filtered_reactions)
+    |> assign(:recognition_reactions_count, length(filtered_reactions))
+    |> assign(:recognition_reactions_status, :loaded)
+  end
+
+  defp assign_recognition_reactions(socket, _assigns) do
+    assign(socket, :recognition_reactions_status, :loading)
   end
 end
--- apps/artemis_web/lib/artemis_web/live/recognition_cards_live.ex
+++ apps/artemis_web/lib/artemis_web/live/recognition_cards_live.ex
@@ -2,6 +2,7 @@ defmodule ArtemisWeb.RecognitionCardsLive do
   use ArtemisWeb.LiveView
 
   alias Artemis.ListComments
+  alias Artemis.ListReactions
 
   # LiveView Callbacks
 
@@ -14,10 +15,12 @@ defmodule ArtemisWeb.RecognitionCardsLive do
     assigns =
       socket
       |> assign(:comments, :loading)
+      |> assign(:reactions, :loading)
       |> assign(:recognitions, recognitions)
       |> assign(:user, user)
 
     if connected?(socket), do: Process.send_after(self(), :list_comments, 10)
+    if connected?(socket), do: Process.send_after(self(), :list_reactions, 10)
 
     :ok = ArtemisPubSub.subscribe(broadcast_topic)
 
@@ -54,6 +57,30 @@ defmodule ArtemisWeb.RecognitionCardsLive do
     comments = maybe_delete_comment(socket, payload)
 
     {:noreply, assign(socket, :comments, comments)}
+  end
+
+  def handle_info(:list_reactions, socket) do
+    reactions = list_reactions(socket)
+
+    {:noreply, assign(socket, :reactions, reactions)}
+  end
+
+  def handle_info(%{event: "reaction:created", payload: payload}, socket) do
+    reactions = maybe_add_reaction(socket, payload)
+
+    {:noreply, assign(socket, :reactions, reactions)}
+  end
+
+  def handle_info(%{event: "reaction:updated", payload: payload}, socket) do
+    reactions = maybe_update_reaction(socket, payload)
+
+    {:noreply, assign(socket, :reactions, reactions)}
+  end
+
+  def handle_info(%{event: "reaction:deleted", payload: payload}, socket) do
+    reactions = maybe_delete_reaction(socket, payload)
+
+    {:noreply, assign(socket, :reactions, reactions)}
   end
 
   def handle_info(_, socket) do
@@ -94,6 +121,38 @@ defmodule ArtemisWeb.RecognitionCardsLive do
     end
   end
 
+  defp list_reactions(socket) do
+    params = %{
+      filters: %{
+        resource_id: get_ids(socket),
+        resource_type: "Recognition"
+      }
+    }
+
+    ListReactions.call(params, socket.assigns.user)
+  end
+
+  defp maybe_add_reaction(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> [payload.data | socket.assigns.reactions]
+      false -> socket.assigns.reactions
+    end
+  end
+
+  defp maybe_update_reaction(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> update_reaction(socket.assigns.reactions, payload.data)
+      false -> socket.assigns.reactions
+    end
+  end
+
+  defp maybe_delete_reaction(socket, payload) do
+    case resource_type_match?(payload) && resource_id_match?(socket, payload) do
+      true -> delete_reaction(socket.assigns.reactions, payload.data)
+      false -> socket.assigns.reactions
+    end
+  end
+
   # Helpers
 
   defp get_ids(socket) do
@@ -118,6 +177,26 @@ defmodule ArtemisWeb.RecognitionCardsLive do
   end
 
   defp delete_comment(current, new) do
+    current
+    |> Enum.reduce([], fn item, acc ->
+      case item.id == new.id do
+        true -> acc
+        false -> [item | acc]
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp update_reaction(current, new) do
+    Enum.map(current, fn item ->
+      case item.id == new.id do
+        true -> new
+        false -> item
+      end
+    end)
+  end
+
+  defp delete_reaction(current, new) do
     current
     |> Enum.reduce([], fn item, acc ->
       case item.id == new.id do
