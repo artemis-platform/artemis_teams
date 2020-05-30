06d85f9c328b0a12aefc199b99f71771c340ea75
Chris Laskey <contact@chrislaskey.com>
2020-05-30 15:18:30 -0400

    Recognition Summary

--- apps/artemis_web/lib/artemis_web/controllers/home_controller.ex
+++ apps/artemis_web/lib/artemis_web/controllers/home_controller.ex
@@ -10,6 +10,7 @@ defmodule ArtemisWeb.HomeController do
 
     assigns = [
       event_templates: get_related_event_templates(user),
+      recognition_totals: get_recognition_totals(user),
       recognitions: get_recognitions(user),
       user_teams: get_related_user_teams(user)
     ]
@@ -30,8 +31,57 @@ defmodule ArtemisWeb.HomeController do
     ListEventTemplates.call(params, user)
   end
 
+  defp get_recognition_totals(user) do
+    params = %{
+      count: true
+    }
+
+    all =
+      params
+      |> ListRecognitions.call(user)
+      |> hd()
+      |> Map.get(:count)
+
+    # From User
+
+    params = %{
+      count: true,
+      filters: %{
+        created_by_id: user.id
+      }
+    }
+
+    from_user =
+      params
+      |> ListRecognitions.call(user)
+      |> hd()
+      |> Map.get(:count)
+
+    # To User
+
+    params = %{
+      count: true,
+      filters: %{
+        user_id: user.id
+      }
+    }
+
+    to_user =
+      params
+      |> ListRecognitions.call(user)
+      |> hd()
+      |> Map.get(:count)
+
+    %{
+      all: all,
+      from_user: from_user,
+      to_user: to_user
+    }
+  end
+
   defp get_recognitions(user) do
     params = %{
+      page_size: 10,
       paginate: true,
       preload: [:created_by, :users]
     }
