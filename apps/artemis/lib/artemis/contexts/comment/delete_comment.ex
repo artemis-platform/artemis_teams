9894d7d3aed0e3797bddc4c2d3306b631d8c9fee
Chris Laskey <contact@chrislaskey.com>
2020-06-01 11:03:15 -0400

    Cleanup associated Reactions when deleting Comments

--- apps/artemis/lib/artemis/contexts/comment/delete_comment.ex
+++ apps/artemis/lib/artemis/contexts/comment/delete_comment.ex
@@ -1,25 +1,40 @@
 defmodule Artemis.DeleteComment do
   use Artemis.Context
 
+  alias Artemis.DeleteManyAssociatedReactions
   alias Artemis.GetComment
   alias Artemis.Repo
 
-  def call!(id, user) do
-    case call(id, user) do
+  def call!(id, params \\ %{}, user) do
+    case call(id, params, user) do
       {:error, _} -> raise(Artemis.Context.Error, "Error deleting comment")
       {:ok, result} -> result
     end
   end
 
-  def call(id, user) do
-    id
-    |> get_record(user)
-    |> delete_record
-    |> Event.broadcast("comment:deleted", user)
+  def call(id, params \\ %{}, user) do
+    with_transaction(fn ->
+      id
+      |> get_record(user)
+      |> delete_associated_reactions(user)
+      |> delete_record
+      |> Event.broadcast("comment:deleted", params, user)
+    end)
   end
 
   def get_record(%{id: id}, user), do: get_record(id, user)
   def get_record(id, user), do: GetComment.call(id, user)
+
+  def delete_associated_reactions(record, user) do
+    resource_type = "Comment"
+    resource_id = record.id
+
+    {:ok, _} = DeleteManyAssociatedReactions.call(resource_type, resource_id, user)
+
+    record
+  rescue
+    _ -> record
+  end
 
   defp delete_record(nil), do: {:error, "Record not found"}
   defp delete_record(record), do: Repo.delete(record)
--- apps/artemis/lib/artemis/contexts/comment/delete_many_associated_comments.ex
+++ apps/artemis/lib/artemis/contexts/comment/delete_many_associated_comments.ex
@@ -1,7 +1,10 @@
 defmodule Artemis.DeleteManyAssociatedComments do
+  use Artemis.Context
+
   import Ecto.Query
 
   alias Artemis.Comment
+  alias Artemis.DeleteManyAssociatedReactions
   alias Artemis.Repo
 
   def call!(resource_type, resource_id \\ nil, user) do
@@ -13,16 +16,35 @@ defmodule Artemis.DeleteManyAssociatedComments do
   end
 
   def call(resource_type, resource_id \\ nil, user) do
-    case delete_records(resource_type, resource_id, user) do
+    case delete(resource_type, resource_id, user) do
       {:error, message} -> {:error, message}
-      {total, _} -> {:ok, %{total: total}}
+      {:ok, {total, _}} -> {:ok, %{total: total}}
     end
   end
 
-  defp delete_records(resource_type, resource_id, _user) do
+  defp delete(resource_type, resource_id, user) do
+    with_transaction(fn ->
+      associated_records = list_records(resource_type, resource_id)
+      associated_ids = Enum.map(associated_records, & &1.id)
+
+      Enum.map(associated_ids, fn associated_id ->
+        {:ok, _} = DeleteManyAssociatedReactions.call("Comment", associated_id, user)
+      end)
+
+      delete_records(associated_ids)
+    end)
+  end
+
+  defp list_records(resource_type, resource_id) do
     Comment
     |> where([c], c.resource_type == ^resource_type)
     |> maybe_where_resource_id(resource_id)
+    |> Repo.all()
+  end
+
+  defp delete_records(comment_ids) do
+    Comment
+    |> where([c], c.id in ^comment_ids)
     |> Repo.delete_all()
   end
 
