ada0fa954cff2fb070672b71d43dfeb174133921
Chris Laskey <contact@chrislaskey.com>
2020-08-05 13:51:47 -0400

    Github Issue Placeholder

--- apps/artemis_web/lib/artemis_web/controllers/github_issue_controller.ex
+++ apps/artemis_web/lib/artemis_web/controllers/github_issue_controller.ex
@@ -18,16 +18,26 @@ defmodule ArtemisWeb.GithubIssueController do
   def index(conn, params) do
     authorize(conn, "github-issues:list", fn ->
       user = current_user(conn)
+      github_repositories = get_github_repositories()
       github_issues_all = ListGithubIssues.call(user)
       github_issues_filtered = ListGithubIssues.call(params, user)
 
       assigns = [
         default_columns: @default_columns,
         github_issues_all: github_issues_all,
-        github_issues_filtered: github_issues_filtered
+        github_issues_filtered: github_issues_filtered,
+        github_repositories: github_repositories
       ]
 
       render_format(conn, "index", assigns)
     end)
+  end
+
+  # Helpers
+
+  defp get_github_repositories() do
+    :artemis
+    |> Application.fetch_env!(:github)
+    |> Keyword.fetch!(:repositories)
   end
 end
