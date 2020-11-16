6eb6f3c81c670f7362fcaf911b76c86f9e6a5214
Chris Laskey <contact@chrislaskey.com>
2020-11-16 13:57:15 -0500

    Hour, minute, and second schedule helpers

--- apps/artemis/lib/artemis/helpers/schedule.ex
+++ apps/artemis/lib/artemis/helpers/schedule.ex
@@ -25,6 +25,75 @@ defmodule Artemis.Helpers.Schedule do
   def decode(%Cocktail.Schedule{} = value), do: value
 
   @doc """
+  Return days of the week from recurrence rule
+  """
+  def days_of_the_week(schedule) do
+    schedule
+    |> get_schedule_validations()
+    |> Artemis.Helpers.deep_get([:day, :days])
+  end
+
+  @doc """
+  Return hours from recurrence rule
+  """
+  def hours(schedule) do
+    schedule
+    |> get_schedule_validations()
+    |> Artemis.Helpers.deep_get([:hour_of_day, :hours], [])
+  end
+
+  @doc """
+  Return hour from recurrence rule
+
+  Note: assumes only one value for a given schedule
+  """
+  def hour(schedule) do
+    schedule
+    |> hours()
+    |> List.first()
+  end
+
+  @doc """
+  Return minutes from recurrence rule
+  """
+  def minutes(schedule) do
+    schedule
+    |> get_schedule_validations()
+    |> Artemis.Helpers.deep_get([:minute_of_hour, :minutes], [])
+  end
+
+  @doc """
+  Return minutes from recurrence rule
+
+  Note: assumes only one value for a given schedule
+  """
+  def minute(schedule) do
+    schedule
+    |> minutes()
+    |> List.first()
+  end
+
+  @doc """
+  Return seconds from recurrence rule
+  """
+  def seconds(schedule) do
+    schedule
+    |> get_schedule_validations()
+    |> Artemis.Helpers.deep_get([:second_of_minute, :seconds], [])
+  end
+
+  @doc """
+  Return seconds from recurrence rule
+
+  Note: assumes only one value for a given schedule
+  """
+  def second(schedule) do
+    schedule
+    |> seconds()
+    |> List.first()
+  end
+
+  @doc """
   Humanize schedule value
   """
   def humanize(%Cocktail.Schedule{} = value) do
@@ -55,5 +124,15 @@ defmodule Artemis.Helpers.Schedule do
 
   def current(schedule, start_time) when is_bitstring(schedule) do
     current(decode(schedule), start_time)
+  end
+
+  # Helpers
+
+  defp get_schedule_validations(schedule) do
+    schedule
+    |> Map.get(:recurrence_rules, [])
+    |> List.first()
+    |> Kernel.||(%{})
+    |> Map.get(:validations, %{})
   end
 end
