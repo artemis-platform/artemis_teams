8ab94689bb84eaa5064d14f02dc8e7803340280b
Chris Laskey <contact@chrislaskey.com>
2020-11-16 11:05:16 -0500

    Add helper for next occurrence

--- apps/artemis_web/lib/artemis_web/views/event_instance_view.ex
+++ apps/artemis_web/lib/artemis_web/views/event_instance_view.ex
@@ -167,6 +167,17 @@ defmodule ArtemisWeb.EventInstanceView do
   end
 
   @doc """
+  Return the current event instance date
+  """
+  def get_next_instance_date(event) do
+    end_of_day = Timex.end_of_day(Timex.now())
+
+    event.schedule
+    |> Artemis.Helpers.Schedule.current(end_of_day)
+    |> Date.to_iso8601()
+  end
+
+  @doc """
   Filter event answers for event question
   """
   def get_event_answers_for_event_question(event_answers, event_question) do
